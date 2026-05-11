e10eff5588dc32e4ee118c0e0907c4cfabd58752
Vincent-HABAYIMANA <vincenthabayimana1@gmail.com>
2026-05-12 01:51:49 +0200

    adding my file code changes

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,5 @@
-export default function handler(req, res) {
-  res.status(200).json({
+export async function GET() {
+  return Response.json({
     success: true,
     message: "Hello from Vercel!",
     time: new Date().toISOString(),
